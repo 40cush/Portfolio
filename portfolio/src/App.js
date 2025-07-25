235ea1870e80665bd49e42509a461fc857e2d53e
Your Name <you@example.com>
2025-07-25 06:18:19 -0700

    today

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import Greeting from './Greeting';
+const name = "cholo"
+const App = () => {
+        return (
+         <div>
+          <p> my name is {name},i am 90 years old and i am learning frontend</p>
+          </div>)
+ };
+export default Greeting;
---
+++ portfolio/src/Greeting.js
@@ -0,0 +1,7 @@
+const name = "cholo"
+const Greeting = (props) => {
+    return (
+        <h2><p>My name is {name} i am 90 years old and im learning frontend</p></h2>
+    )
+};
+export default Greeting;
